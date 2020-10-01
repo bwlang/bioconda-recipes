d676d1ed1494414835621d3d753cf60710812745
Brad Langhorst <langhorst@neb.com>
2020-10-01 16:53:43 -0400

    just make, not make all

--- recipes/bedtk/build.sh
+++ recipes/bedtk/build.sh
@@ -3,6 +3,6 @@
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-make CC=${CC} CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" all
+make CC=${CC} CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}"
 mkdir -p $PREFIX/bin
 cp -f bedtk $PREFIX/bin/
